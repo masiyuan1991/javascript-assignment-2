960910b7915b396653a0d9449244f3dd1512704b
Siyuan Ma <sma24@id.iit.edu>
2017-04-12 17:34:23 -0500

    first draft of assignment

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 // Do your work in this file.
+
+var boxes = document.querySelectorAll(".box");
+var color;
+var n = boxes.length;
+console.log(boxes.length);
+
+
+if (201 <= n && n <= 300) {
+    color = "yellow";
+} else if (301 <= n && n <= 400) {
+    color = "green";
+} else if (401 <= n && n <= 500) {
+    color = "purple";
+} else if (501 <= n && n <= 600) {
+    color = "orange";
+} else if (601 <= n && n <= 700) {
+    color = "pink";
+} else if (701 <= n && n <= 800) {
+    color = "black";
+} else if (801 <= n && n <= 900) {
+    color = "brown";
+} else {
+    color = "blue";
+}
+
+function boxiClicked(){
+    boxes[i].style.backgroundColor = color;
+}
+
+for (var i=0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", boxiClicked);
+}
+
+
+    
+
+
+    // if(boxes[i].className == "box"){
+    //     boxes[i].className = "clicked";
+    // } else {
+    //     boxes[i].className == "box";
+    // }
+
+
+
